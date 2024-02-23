b9dfb46888fc647ef73fc1fef6c2916eafda2386
fawzy20 <fawzy-elsam@outlook.fr>
2024-02-23 21:46:45 +0100

    configuration test unitaire

--- src/test/java/com/example/gestinonnaryTache/GestinonnaryTacheApplicationTests.java
+++ src/test/java/com/example/gestinonnaryTache/GestinonnaryTacheApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.gestinonnaryTache;
-
+import com.example.gestinonnaryTache.model.Utilisateur;
+import com.example.gestinonnaryTache.repository.UtilisateurRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GestinonnaryTacheApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private UtilisateurRepository utilisateurRepository;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private Utilisateur utilisateur;
+
+	Utilisateur utilisateur1 = new Utilisateur("els","test1234","els@outlook.fr","","jlal");
+
 	@Test
-	void contextLoads() {
+	void addUser() throws Exception {
+		String jsonRequest = objectMapper.writeValueAsString(utilisateur1);
+		mockMvc.perform(post("/nouveauCompte")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonRequest))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	void getUsers() throws Exception {
+		mockMvc.perform(get("/users"))
+				.andDo(print())
+				.andExpectAll(status().isOk());
 	}
 
 }
